52b7b5bfd70fc95a199a1ce064698468ce6250ee
Aleshin, Andrei <andrei.aleshin@customertimes.com>
2022-07-29 17:53:12 +0300

    add component UI

--- force-app/main/default/lwc/orderManagement/orderManagement.js
+++ force-app/main/default/lwc/orderManagement/orderManagement.js
@@ -7,7 +7,9 @@ import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 export default class OrderManagement extends LightningElement {
     recordId;
     error;
-
+    products=[{Name:'test name', Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus luctus nulla vel consectetur. Mauris blandit dignissim libero. In sollicitudin lectus id felis scelerisque eleifend. Aliquam tortor elit, lobortis sed condimentum ac, porta bibendum mauris. Vestibulum id iaculis ex. Fusce pharetra ullamcorper ante, eget tempus mi efficitur vitae. Vivamus tempor nisl id sem maximus aliquam.'}, {Name:'test name2', Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus luctus nulla vel consectetur. Mauris blandit dignissim libero. In sollicitudin lectus id felis scelerisque eleifend. Aliquam tortor elit, lobortis sed condimentum ac, porta bibendum mauris. Vestibulum id iaculis ex. Fusce pharetra ullamcorper ante, eget tempus mi efficitur vitae. Vivamus tempor nisl id sem maximus aliquam.'}];
+    types=[{id:'type1', Name:'type1'}, {id:'type2', Name:'type2'}];
+    families=[{id:'fam1', Name: 'fammmmmmmmmmmm1'}, {id:'fam2', Name:'fam2'}];
     @wire(CurrentPageReference)
     handleResult({state}) {
         if (state) {
