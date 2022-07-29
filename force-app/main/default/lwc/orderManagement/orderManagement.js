ffca25cb0b3192876f1a04047ad6ebd612bfc953
Aleshin, Andrei <andrei.aleshin@customertimes.com>
2022-07-29 14:19:43 +0300

    Add showing account information in component

--- force-app/main/default/lwc/orderManagement/orderManagement.js
+++ force-app/main/default/lwc/orderManagement/orderManagement.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import NAME_FIELD from '@salesforce/schema/Account.Name'
+import ACCOUNT_NUMBER_FIELD from '@salesforce/schema/Account.AccountNumber'
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 
-export default class OrderManagement extends LightningElement {}
+export default class OrderManagement extends LightningElement {
+    recordId;
+    error;
+
+    @wire(CurrentPageReference)
+    handleResult({state}) {
+        if (state) {
+            this.recordId = state.c__recordId;
+        }
+        else {
+            error = 'error';
+        }
+    };
+    
+    @wire(getRecord, {recordId:"$recordId", fields:[NAME_FIELD, ACCOUNT_NUMBER_FIELD]})
+    account;
+
+    get name() {
+        return getFieldValue(this.account.data, NAME_FIELD);
+    }
+    get number() {
+        return getFieldValue(this.account.data, ACCOUNT_NUMBER_FIELD);
+    }
+
+    get hasAccountData() {
+        return this.account && this.account.data;
+    }
+}
