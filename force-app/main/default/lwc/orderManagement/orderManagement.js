80a26ca134c965b90d04f37c2f8923912a561b2f
Aleshin, Andrei <andrei.aleshin@customertimes.com>
2022-07-31 19:08:28 +0300

    add filter and search

--- force-app/main/default/lwc/orderManagement/orderManagement.js
+++ force-app/main/default/lwc/orderManagement/orderManagement.js
@@ -1,27 +1,44 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
-import NAME_FIELD from '@salesforce/schema/Account.Name'
-import ACCOUNT_NUMBER_FIELD from '@salesforce/schema/Account.AccountNumber'
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+
+import getProducts from '@salesforce/apex/ProductLoader.getProducts';
+import getTypePicklistValues from '@salesforce/apex/ProductLoader.getTypePicklistValues';
+import getFamilyPicklistValues from '@salesforce/apex/ProductLoader.getFamilyPicklistValues';
+
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import ACCOUNT_NUMBER_FIELD from '@salesforce/schema/Account.AccountNumber';
 
 export default class OrderManagement extends LightningElement {
     recordId;
     error;
-    products=[{Name:'test name', Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus luctus nulla vel consectetur. Mauris blandit dignissim libero. In sollicitudin lectus id felis scelerisque eleifend. Aliquam tortor elit, lobortis sed condimentum ac, porta bibendum mauris. Vestibulum id iaculis ex. Fusce pharetra ullamcorper ante, eget tempus mi efficitur vitae. Vivamus tempor nisl id sem maximus aliquam.'}, {Name:'test name2', Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus luctus nulla vel consectetur. Mauris blandit dignissim libero. In sollicitudin lectus id felis scelerisque eleifend. Aliquam tortor elit, lobortis sed condimentum ac, porta bibendum mauris. Vestibulum id iaculis ex. Fusce pharetra ullamcorper ante, eget tempus mi efficitur vitae. Vivamus tempor nisl id sem maximus aliquam.'}];
-    types=[{id:'type1', Name:'type1'}, {id:'type2', Name:'type2'}];
-    families=[{id:'fam1', Name: 'fammmmmmmmmmmm1'}, {id:'fam2', Name:'fam2'}];
+
+    searchString;
+    selectedType = '%';
+    selectedFamily = '%';
+
+    products;
+
     @wire(CurrentPageReference)
-    handleResult({state}) {
+    handleResult({ state }) {
         if (state) {
             this.recordId = state.c__recordId;
+        } else {
+            this.error = 'error';
         }
-        else {
-            error = 'error';
-        }
-    };
-    
-    @wire(getRecord, {recordId:"$recordId", fields:[NAME_FIELD, ACCOUNT_NUMBER_FIELD]})
+    }
+
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: [NAME_FIELD, ACCOUNT_NUMBER_FIELD]
+    })
     account;
+
+    @wire(getFamilyPicklistValues)
+    _families;
+
+    @wire(getTypePicklistValues)
+    _types;
 
     get name() {
         return getFieldValue(this.account.data, NAME_FIELD);
@@ -29,8 +46,71 @@ export default class OrderManagement extends LightningElement {
     get number() {
         return getFieldValue(this.account.data, ACCOUNT_NUMBER_FIELD);
     }
-
     get hasAccountData() {
         return this.account && this.account.data;
+    }
+
+    get types() {
+        let result = [{ value: '%', label: 'No filter' }];
+        if (this._types.data) {
+            for (let type of this._types.data) {
+                result.push({ value: type, label: type });
+            }
+        }
+        return result;
+    }
+
+    get families() {
+        let result = [{ value: '%', label: 'No filter' }];
+        if (this._families.data) {
+            for (let family of this._families.data) {
+                result.push({ value: family, label: family });
+            }
+        }
+        return result;
+    }
+
+    handleInputChange({ detail: { value } }) {
+        this.searchString = value;
+        this.loadProducts(
+            this.searchString,
+            this.selectedType,
+            this.selectedFamily
+        );
+    }
+
+    handleTypeChange({ target: { value } }) {
+        this.selectedType = value;
+        this.loadProducts(
+            this.searchString,
+            this.selectedType,
+            this.selectedFamily
+        );
+    }
+
+    handleFamilyChange({ target: { value } }) {
+        this.selectedFamily = value;
+        this.loadProducts(
+            this.searchString,
+            this.selectedType,
+            this.selectedFamily
+        );
+    }
+
+    connectedCallback() {
+        this.loadProducts(
+            this.searchString,
+            this.selectedType,
+            this.selectedFamily
+        );
+    }
+
+    async loadProducts(searchString, selectedType, selectedFamily) {
+        const result = await getProducts({
+            searchString,
+            selectedType,
+            selectedFamily
+        });
+        this.products = result.products;
     }
 }
